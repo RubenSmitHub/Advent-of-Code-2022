54c315cb0187df292eb703ff228a1970cf7e381e
Ruben Smit <ruben_19_92@hotmail.com>
2022-12-02 16:56:04 +0100

    Day 1 completed

---
+++ Advent of Code 2022/Day 1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+using Day_1;
+
+string FileContents = File.ReadAllText("input.txt");
+
+List<elf> SantasElfs = new List<elf>();
+
+using (StringReader Reader = new(FileContents))
+{
+  while (true)
+  {
+    string? line = Reader.ReadLine();
+    if (line == null) break;
+
+    int currentCalories = 0;
+
+    elf currentElf = new elf();
+
+    while (int.TryParse(line, out currentCalories))
+    {
+      currentElf.Calories += currentCalories;
+      line = Reader.ReadLine();
+    }
+
+    SantasElfs.Add(currentElf);
+
+  }
+
+  Console.Write("Answer part 1: ");
+  Console.WriteLine(SantasElfs.Max(t => t.Calories));
+
+  Console.Write("Answer part 2: ");
+  Console.WriteLine(SantasElfs.OrderByDescending(t => t.Calories).Take(3).Sum(y => y.Calories));
+
+
+
+}
