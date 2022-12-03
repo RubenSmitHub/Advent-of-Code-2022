c66553b6bab418713b04bd2e3890cca705cc9bec
Ruben Smit <ruben_19_92@hotmail.com>
2022-12-03 13:14:17 +0100

    Update Program.cs
    Solved part 2

--- Advent of Code 2022/Day 3/Program.cs
+++ Advent of Code 2022/Day 3/Program.cs
@@ -2,37 +2,68 @@
 
 string FileContents = File.ReadAllText("input.txt");
 
+int groupsum = 0;
 
-int CommonItemsSum = 0;
-
+// Part 2 solution
 using (StringReader sr = new(FileContents))
 {
   while (true)
   {
-    string? line = sr.ReadLine();
-    if (line == null) break;
+    string? member1 = sr.ReadLine();
+    string? member2 = sr.ReadLine();
+    string? member3 = sr.ReadLine();
+    if (member1 == null) break;
+    if (member2 == null) break;
+    if (member3 == null) break;
 
-
-    string compartiment1 = line.Substring(0, line.Length / 2);
-    string compartiment2 = line[(line.Length / 2)..];
-
-    Console.WriteLine($"{line} -> {compartiment1} | {compartiment2}");
-
-    for (int i = 0; i < compartiment1.Length; i++)
+    for (int i = 0; i < member1?.Length; i++)
     {
-      Char curChar = compartiment1[i];
-
-      if (compartiment2.Contains(curChar,StringComparison.Ordinal))
+      char c = (char)member1[i];
+      if (member2.Contains(c) & member3.Contains(c))
       {
-        CommonItemsSum+= GetCharValue(curChar);
-        compartiment2 = compartiment2.Replace(curChar.ToString(),"");
+        groupsum += GetCharValue(c);
+        break;
       }
     }
   }
-
 }
 
-Console.WriteLine($"Result: {CommonItemsSum}");
+Console.WriteLine($"Result: {groupsum}");
+
+
+
+// PART 1 SOLUTION
+
+// int CommonItemsSum = 0;
+
+//using (StringReader sr = new(FileContents))
+//{
+//  while (true)
+//  {
+//    string? line = sr.ReadLine();
+//    if (line == null) break;
+
+
+//    string compartiment1 = line.Substring(0, line.Length / 2);
+//    string compartiment2 = line[(line.Length / 2)..];
+
+//    Console.WriteLine($"{line} -> {compartiment1} | {compartiment2}");
+
+//    for (int i = 0; i < compartiment1.Length; i++)
+//    {
+//      Char curChar = compartiment1[i];
+
+//      if (compartiment2.Contains(curChar,StringComparison.Ordinal))
+//      {
+//        CommonItemsSum+= GetCharValue(curChar);
+//        compartiment2 = compartiment2.Replace(curChar.ToString(),"");
+//      }
+//    }
+//  }
+
+//}
+
+//Console.WriteLine($"Result: {CommonItemsSum}");
 
 int GetCharValue(char curChar)
 {
