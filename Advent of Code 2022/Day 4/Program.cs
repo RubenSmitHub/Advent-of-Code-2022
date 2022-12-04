15c0c90acc50a32a31c985b671d56a7a0bba00fa
Ruben Smit <ruben_19_92@hotmail.com>
2022-12-04 22:01:39 +0100

    Day 4 wip

---
+++ Advent of Code 2022/Day 4/Program.cs
@@ -0,0 +1,64 @@
+﻿using Day_4;
+
+string FileContents = File.ReadAllText("Input.txt");
+
+int TotalInRange = 0;
+int TotalOverlaps = 0;
+int TotalNotEverlaps = 0;
+int Total = 0;
+
+using (StringReader sr = new(FileContents))
+{
+  while (true)
+  {
+    string? Line = sr.ReadLine();
+    if (Line == null) break;
+
+    string RangeAText = Line.Split(',')[0];
+    string RangeBText = Line.Split(',')[1];
+
+    Day_4.Range a = new()
+    {
+      StartIndex = int.Parse(RangeAText.Split("-")[0]),
+      EndIndex = int.Parse(RangeAText.Split("-")[1])
+    };
+
+    Day_4.Range b = new()
+    {
+      StartIndex = int.Parse(RangeBText.Split("-")[0]),
+      EndIndex = int.Parse(RangeBText.Split("-")[1])
+    };
+
+    if (a.Contains(b) | b.Contains(a))
+    {
+      TotalInRange++;
+    }
+
+    bool IsOverlapping = false;
+    if (a.Overlaps(b))
+    {
+      TotalOverlaps++;
+      IsOverlapping = true;
+    }
+
+    bool IsNotOverlapping = false;
+    if (a.DoNotOverlap(b))
+    {
+      TotalNotEverlaps++;
+      IsNotOverlapping = true;
+    }
+    Total++;
+
+    if ((IsOverlapping & IsNotOverlapping) | (!IsNotOverlapping & !IsOverlapping))
+    {
+      Console.WriteLine("Error in:");
+      Console.WriteLine($"{RangeAText} with {RangeBText} isOverlapping: {IsOverlapping}, isNotOverlapping: {IsNotOverlapping}");
+    }
+  }
+}
+
+Console.WriteLine($"Result Part 1: {TotalInRange}");
+Console.WriteLine();
+Console.WriteLine($"Total not overlaps {TotalNotEverlaps} / {Total}");
+Console.WriteLine($"Total: { TotalOverlaps} / { Total}");
+Console.WriteLine($"Result Part 2: {TotalOverlaps}");
---
+++ Advent of Code 2022/Day 4/Range.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+  internal class Range
+  {
+    public int StartIndex { get; set; } = 0;
+
+    public int EndIndex { get; set; } = 0;
+
+    public bool Contains(Range r)
+    {
+      return (r.StartIndex >= StartIndex & r.EndIndex <= EndIndex);
+
+    }
+
+    public bool Overlaps(Range r)
+    {
+      if (StartIndex >= r.StartIndex & StartIndex <= r.EndIndex)
+      {
+        if (EndIndex >= r.StartIndex & EndIndex <= r.EndIndex)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public bool DoNotOverlap(Range r)
+    {
+      if ((StartIndex < r.StartIndex | StartIndex > r.EndIndex)|
+        (EndIndex < r.StartIndex | EndIndex > r.EndIndex))
+      {
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
